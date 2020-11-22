975fcc38a99bc268d975fb489e1a229a31e442e0
ali <aliahsan365@gmail.com>
2020-11-23 00:07:40 +0100

    subieno librerias en c++ v1

---
+++ c++/librerias/main.cpp
@@ -0,0 +1,14 @@
+
+#include <iostream>
+#include "testclass.h"
+using namespace std;
+
+
+int main() {
+
+	Unuseful u = Unuseful();
+	u.printUnusefulStatement();
+
+
+
+}
---
+++ c++/librerias/static_library/testclass.cpp
@@ -0,0 +1,8 @@
+#include "testclass.h"
+#include <iostream>
+
+
+void Unuseful::printUnusefulStatement()
+{
+    std::cout << "Hello world!" << std::endl;
+}
